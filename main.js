ea2bc1725a212504650c11bea5b2e8eb48848fb8
Jeffersonmk <jefferson20042011@protonmail.com>
2024-03-28 18:45:23 -0300

    mod:index.html, main.css and main.js

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 const form = document.getElementById('form-atividade');
+const imgAprovado = '<img src="./images/aprovado.png" alt= "Emoji Celebrando"/> '
+const imgReprovado = '<img src="./images/reprovado.png" alt= "Emoji Decepcionado"/> '
+
+let linhas = '';
+
 form.addEventListener('submit',function(e) {
-    e.preventDefault;
+    e.preventDefault();
 
     const inputNomeAtividade = document.getElementById('nome-atividade');
     const inputNotaAtividade = document.getElementById('nota-atividade');
 
-    alert('Atividade: ${inputNomeAtividade.value} - Nota ${inputNotaAtividade.value}');
 
+    let linha = '<tr>';
+    linha += `<td>${inputNomeAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`;
+    linha += '</tr>'
+
+    linhas += linha;
+
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+    inputNomeAtividade.value = '';
+    inputNotaAtividade.value = ''; 
 });
-    
